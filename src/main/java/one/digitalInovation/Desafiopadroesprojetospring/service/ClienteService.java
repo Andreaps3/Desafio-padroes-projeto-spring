17fc34d23435e0497d0ad1aeab93b21b294deb95
Andrea Santos <andrea.ps03@gmail.com>
2022-08-08 00:33:12 -0300

    Initial commit

---
+++ src/main/java/one/digitalInovation/Desafiopadroesprojetospring/service/ClienteService.java
@@ -0,0 +1,17 @@
+package one.digitalInovation.Desafiopadroesprojetospring.service;
+
+import one.digitalInovation.Desafiopadroesprojetospring.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
